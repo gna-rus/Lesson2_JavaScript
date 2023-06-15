6b2dcadc9b295bfe5b2d94d911dd8f7e52181e41
gnarus-tm <71601039+gna-rus@users.noreply.github.com>
2023-06-15 09:09:20 +0300

    Add files via upload

---
+++ Task2/assets/script.js
@@ -0,0 +1,36 @@
+
+        function Hello_Client(name)
+        {
+            alert(`Привет, ${name}`);
+        }
+
+        function Heloo_Client_Age(age_client)
+        {
+            alert(`Ухты! Тебе целых ${age_client} лет`);
+        }
+
+        let nums = parseInt(prompt("1 - Введите свое ФИО; \n2 - Введите свой возраст."));
+
+        switch (nums){
+        case 1:
+            let name_client = prompt("Введите Ваше ФИО");
+            Hello_Client(name_client);
+            break;
+
+        case 2:
+            let age_client = parseInt(prompt("Введите Ваш возраст"));
+            Heloo_Client_Age(age_client);
+            break;
+
+        default:
+            alert("Тебе русским языком сказали что вводить 1 или 2");
+        }
+
+
+
+
+
+        
+
+        
+
