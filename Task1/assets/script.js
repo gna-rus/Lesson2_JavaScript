e91f1eccc6912a68dcaa6290e870914db9f73e4d
gna-rus <gnarus@inbox.ru>
2023-06-15 09:17:05 +0300

    first commit

---
+++ Task1/assets/script.js
@@ -0,0 +1,18 @@
+
+        function Hello_Client(name)
+        {
+            alert(`Привет, ${name}`);
+        }
+
+
+        let name_client = prompt("Введите Ваше ФИО");
+        Hello_Client(name_client);
+
+
+
+
+
+        
+
+        
+
